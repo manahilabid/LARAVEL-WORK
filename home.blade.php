c685850d3c86b3d9a83f8a750e11b677b77c8100
MANAHIL ABID <soniaabid806@gmail.com>
2025-05-22 09:34:35 -0700

    folder structure in laravel

---
+++ home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    <h3>welcome to home page in laravel</h3>
+</body>
+</html>
+@php
+echo "first code of php in laravel";
+
+    
+$username = "Manahil";
+$email = "Manahil@gmail.com";
+
+@endphp
+<h3>{{$username}}</h3>  
+<h3>{{$email}}</h3>
+
+@if($username = "Manahil")
+<h3>the condition is true</h3>
+
+@else
+<h3>the condition is false</h3>
+@endif
